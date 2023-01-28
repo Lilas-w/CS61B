608ca7140d3a4f02864e2aa4f8084b766382471a
Lilas-w <lilas0617@outlook.com>
2023-01-28 14:57:05 +0800

    proj0: emptySpaceExists completed

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -138,6 +138,14 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        int size = b.size();
+        for (int i = 0;i < size;i += 1){
+            for(int j = 0;j < size;j += 1){
+                if(b.tile(i, j) == null){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
